598331f4bd9fdc5e650c91fa6ffa703eee8c2172
ValerioMik <valerio.avallone@hotmail.com>
2021-11-10 14:47:42 +0100

    esercizio piu bonus terminato

---
+++ main.js
@@ -0,0 +1,15 @@
+/* Descrizione:
+Stampare a schermo un messaggio all’interno di un h1,
+utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+var eseRcizio = new Vue({
+    el:'#container',
+    data: {
+    message:'questo nuovo frame framework mi sta piacendo per ora',
+    immagine:'https://unsplash.it/300/300?image='
+    },
+});
+
+
