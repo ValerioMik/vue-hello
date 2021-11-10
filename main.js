07eb1b4395607846fe09e578d321bd77fd987a29
ValerioMik <valerio.avallone@hotmail.com>
2021-11-10 16:47:00 +0100

    qualche bonus di esperimento

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
 var eseRcizio = new Vue({
     el:'#container',
     data: {
-    message:'questo nuovo frame framework mi sta piacendo per ora',
-    immagine:'https://unsplash.it/300/300?image='
+        message:'Questo nuovo  framework mi sta piacendo per ora',
+        immagine:'https://unsplash.it/300/300?image=',
+        miaclss:'red'
     },
+    methods:{
+       cambiaImg: function(){
+        this.immagine ="https://unsplash.it/g/640/425"
+        } 
+    } 
 });
 
 
